64bf560669db3155fb725d65e2013ed336bb7fb3
liCells <lizong20001204@gmail.com>
2021-07-16 16:57:24 +0800

    完善

---
+++ data.js
@@ -0,0 +1,54 @@
+let commonlyUsedUrls = [
+    {
+        title: 'github',
+        url: 'https://github.com/'
+    },
+    {
+        title: 'gitee',
+        url: 'https://gitee.com/'
+    },
+    {
+        title: 'DB-Engines',
+        url: 'https://db-engines.com/en/'
+    },
+    {
+        title: '美团技术团队',
+        url: 'https://tech.meituan.com/'
+    },
+    {
+        title: 'ThoughtWorks洞见',
+        url: 'https://insights.thoughtworks.cn/'
+    },
+    {
+        title: '中国科技大学开源镜像',
+        url: 'http://mirrors.ustc.edu.cn/'
+    },
+    {
+        title: '清华大学开源软件镜像站',
+        url: 'https://mirrors.tuna.tsinghua.edu.cn/#'
+    },
+    {
+        title: '华为开源镜像站',
+        url: 'https://mirrors.huaweicloud.com/home'
+    },
+    {
+        title: 'programming-idioms 学习新语言',
+        url: 'https://programming-idioms.org/about#about-block-cheatsheets'
+    },
+    {
+        title: 'thoughtworks 探索雷达',
+        url: 'https://www.thoughtworks.com/cn/radar'
+    },
+    {
+        title: '芒格学院',
+        url: 'https://www.madewill.com/'
+    },
+    {
+        title: 'linux man-pages-zh: Chinese Manual Pages',
+        url: 'https://github.com/man-pages-zh/manpages-zh'
+    },
+    {
+        title: '',
+        url: ''
+    },
+]
--- popup.js
+++ popup.js
@@ -1,4 +1,7 @@
 let symbol = '1'
+let position = -1
+let optionIdPrefix = 'option-'
+let ignoreKeys = [37, 38, 39, 40, 13, 27]
 
 // 获取输入框
 let searchObj = document.getElementById("search")
@@ -10,30 +13,18 @@ searchObj.focus()
 searchObj.onkeyup = search
 
 // 处理所有书签
-const bookmarks = new Map()
-let keys = []
+const bookmarks = []
 // 获取书签
 chrome.bookmarks.getTree(function (bookmarkArray) {
     buildList(bookmarkArray)
-    let li = ''
-    for (let i = 0; i < 10; i++) {
-        li += "<li style='overflow:hidden; text-overflow: ellipsis; white-space: nowrap;'>" + keys[i] + "</li>"
-    }
-    dataListObj.innerHTML = li
-    let dataObj = document.getElementsByTagName("li")
-    for (let i = 0; i < dataObj.length; i++) {
-        dataObj[i].addEventListener("click", function () {
-            searchObj.value = this.innerText
-        })
-    }
+    buildList(commonlyUsedUrls)
 });
 
 // 递归处理书签
 function buildList(val) {
     for (let i = 0; i < val.length; i++) {
         if (val[i].url !== undefined) {
-            bookmarks.set(val[i].title, val[i].url)
-            keys.push(val[i].title)
+            bookmarks.push({title: val[i].title, url: val[i].url})
         }
         if (val[i].children !== undefined) {
             buildList(val[i].children)
@@ -44,7 +35,42 @@ function buildList(val) {
 // 搜索事件
 function search() {
     let val = searchObj.value
+
+    if (event.keyCode === 27) {
+        dataListObj.innerHTML = ''
+        searchObj.value = ''
+        return
+    }
+
+    if (event.keyCode === 38) {
+        try {
+            document.getElementById(optionIdPrefix + position).style.color = ''
+            let dataObj = document.getElementById(optionIdPrefix + --position)
+            dataObj.style.color = 'red'
+            if (dataObj.innerHTML !== '' && dataObj.innerHTML !== undefined) {
+                searchObj.value = dataObj.innerHTML
+            }
+        } catch (e) {
+
+        }
+    } else if (event.keyCode === 40) {
+        if (position === document.getElementsByTagName("li").length - 1) {
+            return
+        }
+        try {
+            document.getElementById(optionIdPrefix + position).style.color = ''
+        } catch (e) {
+
+        }
+        let dataObj = document.getElementById(optionIdPrefix + ++position)
+        dataObj.style.color = 'red'
+
+        if (dataObj.innerHTML !== '' && dataObj.innerHTML !== undefined) {
+            searchObj.value = dataObj.innerHTML
+        }
+    }
     if (val === '') {
+        dataListObj.innerHTML = ''
         return
     }
 
@@ -58,11 +84,28 @@ function search() {
     }
 
     if (event.keyCode === 13 && event.shiftKey) {
+        location.href = document.getElementById(optionIdPrefix + position).getAttribute('value')
     }
 
-    for (let i = 0; i < keys.length; i++) {
-        if (keys[i].indexOf(val) !== -1) {
-            // console.log(keys[i])
+    if (ignoreKeys.indexOf(event.keyCode) === -1) {
+        position = -1
+        let li = ''
+        let sum = 0
+        for (let i = 0; i < bookmarks.length; i++) {
+            if (bookmarks[i].title.toLowerCase().indexOf(val.toLowerCase()) > -1) {
+                li += "<li style='overflow:hidden; text-overflow: ellipsis; white-space: nowrap;' id='" + optionIdPrefix + sum + "' value='" + bookmarks[i].url + "'>" + bookmarks[i].title + "</li>"
+                sum++
+            }
+            if (sum === 10) {
+                break
+            }
+        }
+        dataListObj.innerHTML = li
+        let dataObj = document.getElementsByTagName("li")
+        for (let i = 0; i < dataObj.length; i++) {
+            dataObj[i].addEventListener("click", function () {
+                searchObj.value = this.innerText
+            })
         }
     }
 }
